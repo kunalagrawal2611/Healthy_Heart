926d5d42920cd52376b644417cedda0a1a717293
Kunal Agrawal <kunalagarwal2611@gmail.com>
2023-03-15 16:13:38 -0400

    Reusable Video Player Functionality for all the videos

--- app/src/main/java/com/example/healthyheart/Breathing_activity.java
+++ app/src/main/java/com/example/healthyheart/Breathing_activity.java
@@ -9,7 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Breathing_activity extends AppCompatActivity {
 
-    private ImageView breathing_act_1;
+    private ImageView breathing_act_1, breathing_act_2, breathing_act_3, breathing_act_4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -17,10 +17,52 @@ public class Breathing_activity extends AppCompatActivity {
         setContentView(R.layout.breathing);
 
         breathing_act_1 = (ImageView) findViewById(R.id.imageView_breath_act1);
+        breathing_act_2 = (ImageView) findViewById(R.id.imageView_breath_act2);
+        breathing_act_3 = (ImageView) findViewById(R.id.imageView_breath_act3);
+        breathing_act_4 = (ImageView) findViewById(R.id.imageView_breath_act4);
+        //Video_player video_number = new Video_player();
+        //video_number.play_video(1);
         breathing_act_1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 Intent intent = new Intent(getApplicationContext(), Video_player.class);
+                Bundle b = new Bundle();
+                b.putInt("key",1);
+                intent.putExtras(b);
+                startActivity(intent);
+            }
+        });
+        breathing_act_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getApplicationContext(), Video_player.class);
+                Bundle b = new Bundle();
+                b.putInt("key",2);
+                intent.putExtras(b);
+                startActivity(intent);
+            }
+        });
+        breathing_act_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getApplicationContext(), Video_player.class);
+                Bundle b = new Bundle();
+                b.putInt("key",3);
+                intent.putExtras(b);
+                startActivity(intent);
+            }
+        });
+        breathing_act_4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getApplicationContext(), Video_player.class);
+                Bundle b = new Bundle();
+                b.putInt("key",4);
+                intent.putExtras(b);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/healthyheart/Music.java
+++ app/src/main/java/com/example/healthyheart/Music.java
@@ -1,15 +1,81 @@
 package com.example.healthyheart;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Music extends AppCompatActivity {
+
+    private ImageView music_activity_1, music_activity_2, music_activity_3, music_activity_4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.music);
+
+        music_activity_1 = (ImageView) findViewById(R.id.imageView_music_act1);
+        music_activity_2 = (ImageView) findViewById(R.id.imageView_music_act2);
+        music_activity_3 = (ImageView) findViewById(R.id.imageView_music_act3);
+        music_activity_4 = (ImageView) findViewById(R.id.imageView_music_act4);
+
+        music_activity_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getApplicationContext(), Video_player.class);
+                Bundle b = new Bundle();
+                b.putInt("key",5);
+                intent.putExtras(b);
+                startActivity(intent);
+            }
+        });
+        music_activity_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getApplicationContext(), Video_player.class);
+                Bundle b = new Bundle();
+                b.putInt("key",5);
+                intent.putExtras(b);
+                startActivity(intent);
+            }
+        });music_activity_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getApplicationContext(), Video_player.class);
+                Bundle b = new Bundle();
+                b.putInt("key",6);
+                intent.putExtras(b);
+                startActivity(intent);
+            }
+        });
+        music_activity_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getApplicationContext(), Video_player.class);
+                Bundle b = new Bundle();
+                b.putInt("key",7);
+                intent.putExtras(b);
+                startActivity(intent);
+            }
+        });
+        music_activity_4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getApplicationContext(), Video_player.class);
+                Bundle b = new Bundle();
+                b.putInt("key",8);
+                intent.putExtras(b);
+                startActivity(intent);
+            }
+        });
 
     }
 }
